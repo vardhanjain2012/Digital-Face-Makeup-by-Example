9ed02582d5bdd441bebe887aff62dae5cae5f78b
Vardhan Jain <vardhanjain2012@gmail.com>
2021-04-07 16:56:17 +0530

    xDoG

--- xDog.py
+++ xDog.py
@@ -9,47 +9,48 @@ img = cv2.imread("./sampleImages/s1.jpg", cv2.IMREAD_GRAYSCALE)
 
 # cv2.imshow("Cute Kitens", img)
 
-low_sigma = cv2.GaussianBlur(img,(5,5), 1)
+
+sigma = 0.8
+k = 1.6
+
+low_sigma = cv2.GaussianBlur(img, (0, 0), sigma)
 
 # cv2.imshow("low_sigma", low_sigma)
 
-high_sigma = cv2.GaussianBlur(img,(5,5), 1.6)
+high_sigma = cv2.GaussianBlur(img, (0, 0), sigma*k)
 
 # cv2.imshow("high_sigma", high_sigma)
 
 dog = low_sigma - high_sigma
 
-cv2.imshow("dog", dog)
+# cv2.imshow("dog", dog)
 
 tau = 0.98
 
-xdog = low_sigma - tau*high_sigma
+xdog1 = low_sigma - tau*high_sigma
 
-cv2.imshow("xdog", xdog)
+rho = 500.7
+
+xdog2 = low_sigma + rho*dog 
+
+# cv2.imshow("xdog", xdog)
 
 maxI = 255
 
 def threshold(src, threshold, ):
 	return np.uint8((src>=(maxI*threshold))*maxI)
 
-def softThreshold(src, threshold, psi):
-	return np.uint8(np.add((src>=(maxI*threshold))*maxI, (src<(maxI*threshold))*(np.add(np.ones(src.shape), np.tanh(psi*((src/maxI) - threshold))))*maxI))
-
-def blurred(src):
-	return
-
-def scaledDog(src):
-	return
-
-def sharpenedDog(src):
-	return
+def softThreshold(src, threshold, phi):
+	return np.uint8(np.add((src>=(maxI*threshold))*maxI, (src<(maxI*threshold))*(np.add(np.ones(src.shape), np.tanh(phi*((src/maxI) - threshold))))*maxI))
 
 
-thresholded = threshold(dog, 0.99)
+epsilon = 0.99
+phi = 0.5
+thresholded = threshold(xdog1, epsilon)
 
 cv2.imshow("thresholded", thresholded)
 
-softThresholded = softThreshold(xdog, 0.5, 2.0)
+softThresholded = softThreshold(xdog2, epsilon, phi)
 
 cv2.imshow("softThresholded", softThresholded)
 
